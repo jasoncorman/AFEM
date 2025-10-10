906ceec409ac03a1fdc9a515aa6202865b60b6bf
jcorman3 <jcorman3@gatech.edu>
2025-10-10 10:09:49 -0400

    fix(viewer): Updated Viewer object for pythonocc
    - [x] Moved AppFrame definition into afem display
    - [x] Explicitly integrated wx to Viewer

--- afem/graphics/display.py
+++ afem/graphics/display.py
@@ -20,10 +20,12 @@ import os
 
 from OCC.Core.TopoDS import TopoDS_Shape
 from OCC.Display.wxDisplay import wxViewer3d
-from OCC.Display.SimpleGui import init_display
+from OCC.Display.SimpleGui import check_callable
+from typing import Callable, Optional, Tuple, List
 
 from afem.base.entities import ViewableItem
 from afem.structure.group import Group
+import wx
 
 
 __all__ = ["Viewer"]
@@ -32,16 +34,98 @@ __all__ = ["Viewer"]
 _icon = os.path.dirname(__file__) + '/resources/main.png'
 
 
+# Copying creation of AppFrame from OCC.Display.SimpleGui.init_display
+class AppFrame(wx.Frame):
+    def __init__(self, parent, size, title='AFEM'):
+        wx.Frame.__init__(
+            self,
+            parent,
+            -1,
+            title=title,
+            style=wx.DEFAULT_FRAME_STYLE,
+            size=size,
+        )
+        self.canva = wxViewer3d(self)
+        self.menuBar = wx.MenuBar()
+        self._menus = {}
+        self._menu_methods = {}
+
+    def add_menu(self, menu_name: str) -> None:
+        _menu = wx.Menu()
+        self.menuBar.Append(_menu, f"&{menu_name}")
+        self.SetMenuBar(self.menuBar)
+        self._menus[menu_name] = _menu
+
+    def add_function_to_menu(self, menu_name: str, _callable: Callable) -> None:
+        # point on curve
+        _id = wx.NewId()
+        check_callable(_callable)
+        if menu_name not in self._menus:
+            raise ValueError(f"the menu item {menu_name} does not exist")
+        self._menus[menu_name].Append(
+            _id, _callable.__name__.replace("_", " ").lower()
+        )
+        self.Bind(wx.EVT_MENU, _callable, id=_id)
+
+
 class Viewer(wxViewer3d):
     """
     Simple tool for viewing entities.
     """
 
-    def __init__(self):
-        super(Viewer, self).__init__()
-        self.SetLabel('AFEM')
+    def __init__(
+            self,
+            size: Optional[Tuple[int, int]] = (1024, 768),
+            display_triedron: Optional[bool] = True,
+            background_gradient_color1: Optional[List[int]] = [206, 215, 222],
+            background_gradient_color2: Optional[List[int]] = [128, 128, 128],
+            label: Optional[str] = 'AFEM',
+            show: Optional[bool] = False,
+    ):
+        app = wx.App(False)
+        self._app = app
+        self.win = AppFrame(None, size)
+        self.win.Show(show)
+        wx.SafeYield()
+        self.win.canva.InitDriver()
+        self._app.SetTopWindow(self.win)
+        self.display = self.win.canva._display
+        self.win.SetLabel(label)
+        self.add_key(ord('C'), self.hide)
+        if display_triedron:
+            self.display.display_triedron()
 
-    def display_item(self, item):
+        if background_gradient_color1 and background_gradient_color2:
+            # background gradient
+            self.display.set_bg_gradient_color(
+                background_gradient_color1, background_gradient_color2
+            )
+
+    def start(self):
+        self.win.Show()
+        self._app.MainLoop()
+
+    def hide(self):
+        self.win.Hide()
+        self._app.ExitMainLoop()
+
+    def add_menu(self, *args, **kwargs) -> None:
+        self.win.add_menu(*args, **kwargs)
+
+    def add_function_to_menu(
+            self, *args, **kwargs) -> None:
+        self.win.add_function_to_menu(*args, **kwargs)
+
+    def add_key(self, key: int, fcn: Callable):
+        self.win.canva._key_map[key] = fcn
+
+    def display_item(
+            self,
+            item,
+            material=None,
+            texture=None,
+            update=False
+    ):
         """
         Display a type derived from ``ViewableItem``.
 
@@ -50,8 +134,14 @@ class Viewer(wxViewer3d):
         :return: The AIS_Shape created for the item.
         :rtype: OCC.Core.AIS.AIS_Shape
         """
-        return self.display_shape(item.displayed_shape, item.color,
-                                  item.transparency)
+        return self.display.DisplayShape(
+            item.displayed_shape,
+            material=material,
+            texture=texture,
+            color=item.color,
+            transparency=item.transparency,
+            update=update,
+        )
 
     def display_group(self, group, include_subgroup=True):
         """
@@ -83,4 +173,4 @@ class Viewer(wxViewer3d):
             elif isinstance(item, Group):
                 self.display_group(item)
             elif isinstance(item, TopoDS_Shape):
-                self.display_shape(item)
+                self.display.DisplayShape(item)
